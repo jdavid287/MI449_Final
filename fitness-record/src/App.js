850551263e01595c558d2f649f62450e5c4e6c7c
Jack Davidson <133827574+jdavid287@users.noreply.github.com>
2024-04-21 22:08:24 +0000

    added react

---
+++ fitness-record/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import 
+
+function Fitnesstrack() {
+  return (
+    <p>Fitness Tracker</p>
+  );
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Fitnesstrack />
+      </header>
+    </div>
+  );
+}
+
+export default App;
